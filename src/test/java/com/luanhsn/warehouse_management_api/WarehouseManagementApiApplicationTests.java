bfe15e5b6e22c520793e290658d045e9ffbaabfe
Luanhsn <luan.hyseni09@gmail.com>
2026-06-13 13:14:12 +0200

    chore: initialize spring setup

---
+++ src/test/java/com/luanhsn/warehouse_management_api/WarehouseManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanhsn.warehouse_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
