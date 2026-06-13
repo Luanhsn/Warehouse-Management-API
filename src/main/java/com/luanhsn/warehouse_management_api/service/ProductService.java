dea944ae1bb8a9af09e893b2c869bd6628f2b187
Luanhsn <luan.hyseni09@gmail.com>
2026-06-13 21:12:21 +0200

    chore: initialize ProductService

---
+++ src/main/java/com/luanhsn/warehouse_management_api/service/ProductService.java
@@ -0,0 +1,31 @@
+package com.luanhsn.warehouse_management_api.service;
+
+import com.luanhsn.warehouse_management_api.model.Product;
+import com.luanhsn.warehouse_management_api.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow();
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
