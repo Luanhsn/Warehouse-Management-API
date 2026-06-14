ca0eb4050f7b1474283f00cc8cf9b0e3f5c684fe
Luanhsn <luan.hyseni09@gmail.com>
2026-06-14 15:15:37 +0200

    feat: add exception handling with ResourceNotFoundException and GlobalExceptionhandler

--- src/main/java/com/luanhsn/warehouse_management_api/service/ProductService.java
+++ src/main/java/com/luanhsn/warehouse_management_api/service/ProductService.java
@@ -13,12 +13,23 @@ public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Product existing = productRepository.findById(id).
+        existing.setName(updatedProduct.getName());
+        existing.setDescription(updatedProduct.getDescription());
+        existing.setQuantity(updatedProduct.getQuantity());
+        existing.setPrice(updatedProduct.getPrice());
+        existing.setSku(updatedProduct.getSku());
+        return productRepository.save(existing);
+    }
+
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
     public Product getProductById(Long id) {
-        return productRepository.findById(id).orElseThrow();
+        return productRepository.findById(id).
+                orElseThrow(() -> new ResourceNotFoundException("Product with id " + id + " not found"));
     }
 
     public Product createProduct(Product product) {
